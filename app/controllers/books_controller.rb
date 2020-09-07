5208ca5b701e681d624d823fcb0f66a04a1751a3
simeo russo <simeo94@hotmail.com>
2020-09-07 21:16:07 +0100

    created seed and index

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+
+Book.create(
+  name: Faker::Book.title,
+  auther: Faker::Book.author,
+  rating: rand(1..5)
+  )
+puts "created record"
+end
+
+puts "completed seed"
